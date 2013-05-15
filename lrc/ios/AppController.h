243eed8349d32d47d9009d08db0bdfee963858bf
JuhnXu <JuhnXu@qq.com>
2013-05-15 15:41:31 +0800

    Initial Commit

---
+++ lrc/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  lrcAppController.h
+//  lrc
+//
+//  Created by JuhnXu on 13-5-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
